24edceafec5caaaeb7a1ed854f6568f573eafcb5
Low Level M <lowlevelm@protonmail.com>
2019-11-21 15:23:40 -0800

    Add comments

--- item_manager.py
+++ item_manager.py
@@ -2,52 +2,70 @@ import time
 import json
 
 class Items:
+    """Manage and keep track of items"""
+
     def __init__(self):
         self.queue_items = {}
         self.inprogress_items = {}
         self.done_items = {}
+
+        # Assign only one id per item
         self.current_id = 0
 
     def loadfile(self, file):
+        # Open a csv file, and create one item per line
+
         with open(file, 'r') as jf:
             for line in jf.readlines():
+
+                # Remove newline character from line
                 line = line.replace('\n', '')
 
+                # If line is not a comment, or empty
                 if line.startswith('#') == False and line != '':
+
+                    # Create item
                     self.queue_items[self.current_id] = {
                         'id': self.current_id,
                         'values': line.split(',')
                     }
-                    self.current_id += 1
+
+                    self.current_id += 1 # Add one to the current id
 
     def getitem(self, username, ip):
+        """Gets an item, and moves it to inprogress_items"""
+
         try:
+            # Get item with the lowest id from queue_items
             id = min(self.queue_items, key=int)
             item = self.queue_items.pop(id)
 
-            item['username'] = username
-            item['ip'] = ip
+            # Add item to inprogress_items
+            item['username'] = username # Log username
+            item['ip'] = ip # Log ip
             item['times'] = {}
-            item['times']['starttime'] = int(time.time())
-
+            item['times']['starttime'] = int(time.time()) # Log start time
             self.inprogress_items[id] = item
+
             print(f'giving id {id} to {username}')
+
+            # Return json of item
             return json.dumps({'id': item['id'], 'values': item['values']})
 
-        except ValueError:
+        except ValueError: # no items left
             return 'NoItemsLeft'
 
     def finishitem(self, id, ip):
+        """Gets an item in progress, and moves it to done_items"""
         try:
-            id = int(id)
-            item = self.inprogress_items[id]
+            id = int(id) # Convert item to an integer
+            item = self.inprogress_items[id] # Get item from inprogress_items
 
-            if item['ip'] == ip:
-                self.inprogress_items.pop(id)
+            if item['ip'] == ip: # Check if ip is the same as requester
+                self.inprogress_items.pop(id) # remove item from inprogress_items
+                self.done_items[id] = item # add item to done_items
 
                 print(f"{item['username']} finished {id}")
-                self.done_items[id] = item
-
                 return 'Success'
 
             else:
--- server.py
+++ server.py
@@ -4,46 +4,58 @@ import tornado.ioloop
 import item_manager
 
 
-class basicrh(tornado.web.RequestHandler):
+class homepage(tornado.web.RequestHandler):
+    """This will eventually serve the tracker homepage"""
     def get(self):
         self.write('Hello World')
 
 class start_item(tornado.web.RequestHandler):
+    """API endpoint for requesting an item"""
+
     def get(self):
-        username = self.get_argument('username')
+        username = self.get_argument('username') # Get url parameter
+
+        # Get item from item_manager
         item = items.getitem(username, self.request.remote_ip)
 
         if item == 'NoItemsLeft':
-            self.set_status(404)
+            self.set_status(404) # return 403 forbidden
 
-        self.write(item)
+        self.write(item) # Respond with the output from item_manager
 
 
 class finish_item(tornado.web.RequestHandler):
+    """API endpoint for finishing an item"""
+
     def get(self):
-        id = self.get_argument('id')
+        id = self.get_argument('id') # Get url parameter
+
+        # Tell item_manager to finish item
         done_stat = items.finishitem(id, self.request.remote_ip)
 
+        # If there is an error finishing item
         if done_stat in ['IpDoesNotMatch', 'InvalidID']:
-            self.set_status(403)
+            self.set_status(403) # return 403 forbidden
 
-        self.write(str(done_stat))
+        self.write(str(done_stat)) # Respond with the output from item_manager
 
 
-items = item_manager.Items()
-items.loadfile('items.txt')
+items = item_manager.Items() # create items object
+items.loadfile('items.txt') # Load items
 
 
 if __name__ == "__main__":
-    PORT = 80
+    PORT = 80 # Set server port
 
+    # Create url paths
     app = tornado.web.Application([
-        (r'/', basicrh),
+        (r'/', homepage),
+
+        # API urls
         (r'/item/get', start_item),
         (r'/item/done', finish_item)
     ])
 
-
-    app.listen(PORT)
+    app.listen(PORT) #start listening on PORT
     print(f'Listening on {PORT}')
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.current().start() # Start server
